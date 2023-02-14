23d69870ddbad2cc3e6bab85d12370379135c670
rubeng0nzalez <rgonzalez.mywork@gmail.com>
2023-02-14 16:31:31 -0500

    Update main.c

--- GPIO_Interruption/main/main.c
+++ GPIO_Interruption/main/main.c
@@ -3,16 +3,26 @@
 #include <unistd.h>
 #include "driver/gpio.h"
 
+/*Good practice: Define before with a name the pins you will use and its purpose
+ *
+ * for instance:
+ *
+ * #define LED_PIN	GPIO_NUM_2
+ *
+ * */
+
+#define ESP_LED_PIN	GPIO_NUM_2
+
 void app_main(void)
 {
     printf("This is a simple GPIO example!\n  ");
-	gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+	gpio_set_direction(ESP_LED_PIN, GPIO_MODE_OUTPUT);
     int value = 0;
-    gpio_set_level(GPIO_NUM_2, 0);
+    gpio_set_level(ESP_LED_PIN, 0);
 
     while (true) {
     	if(value == 0){
-    		gpio_set_level(GPIO_NUM_2, 1);
+    		gpio_set_level(ESP_LED_PIN, 1);
     		value = 1;
     	}
     	else
