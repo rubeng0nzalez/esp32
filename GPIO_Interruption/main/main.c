55b113934cc9b2f73f3d1636dbb410557b1cd334
rubeng0nzalez <rgonzalez.mywork@gmail.com>
2023-02-14 16:06:19 -0500

    Flashing the led PIN 2

--- GPIO_Interruption/main/main.c
+++ GPIO_Interruption/main/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include "driver/gpio.h"
 
 void app_main(void)
 {
+    printf("This is a simple GPIO example!\n");
+	gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
+    int value = 0;
+    gpio_set_level(GPIO_NUM_2, 0);
+
     while (true) {
-        printf("This example allows the use of a GPIO Interruption!\n");
+    	if(value == 0){
+    		gpio_set_level(GPIO_NUM_2, 1);
+    		value = 1;
+    	}
+    	else
+    	{
+    		gpio_set_level(GPIO_NUM_2, 0);
+    		value = 0;
+    	}
         sleep(1);
     }
 }
