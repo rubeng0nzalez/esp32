304cfb98b4a262e8dc0fdfed9032182f881811db
rubeng0nzalez <rgonzalez.mywork@gmail.com>
2023-03-16 23:50:12 -0400

    Update main.c
    Using FreeRTOS instead of sleep

--- HelloWorldFromESP32/main/main.c
+++ HelloWorldFromESP32/main/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include "freertos/freeRTOS.h"
 
 void app_main(void)
 {
     while (true) {
         printf("Hello World from ESP32!\n");
         pritnf("This is just for testing purposes\n")
-        sleep(1);
+        vTaskDelay(1);
+        //sleep(1);
     }
 }
