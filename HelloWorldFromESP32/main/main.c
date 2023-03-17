bacd42d63e07fcadcb860098d32e88f915837dbe
rubeng0nzalez <rgonzalez.mywork@gmail.com>
2023-03-16 23:50:31 -0400

    Update main.c
    Fixing ; missing

--- HelloWorldFromESP32/main/main.c
+++ HelloWorldFromESP32/main/main.c
@@ -7,7 +7,7 @@ void app_main(void)
 {
     while (true) {
         printf("Hello World from ESP32!\n");
-        pritnf("This is just for testing purposes\n")
+        pritnf("This is just for testing purposes\n");
         vTaskDelay(1);
         //sleep(1);
     }
